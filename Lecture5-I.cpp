5ecfa6cdfc16bc742b391aaf01db665b5d85efd4
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-08 11:47:14 +0530

    Add files via upload

---
+++ Lecture5-I.cpp
@@ -0,0 +1,15 @@
+// Lecture 5 I
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int f[5]; // learn how just declare the array
+
+    int a, b, c, d, e;
+    cout << "Enter the 5 integers : ";
+    cin >> a >> b >> c >> d >> e; 
+
+    cout << "The integer in reverse order is : " <<  e << d << c << b << a;
+    return 0;
+}
